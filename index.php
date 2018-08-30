9e40bbb881c7bd498350e00c8cabd16c8f8aef35
sme1496 <s.tachi@neptune.kanazawa-it.ac.jp>
2018-08-31 02:00:21 +0900

    add update0831 file

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-    // Composerでインストールしたライブラリを一括読み込み
-    require_once __DIR__ . '/vendor/autoload.php';
-	
-	// POSTメソッドで渡される値を取得、表示
-	$inputString = file_get_contents('php://input');
-	error_log($inputString);
+  echo "Hello world!";
 ?>
